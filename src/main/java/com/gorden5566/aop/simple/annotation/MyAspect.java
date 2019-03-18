9aaed453f6c116f13c64c3ff0ad94cc6fd068a35
gorden5566 <gorden5566@126.com>
2019-03-18 12:47:23 +0800

    SimpleAop:自定义注解

---
+++ src/main/java/com/gorden5566/aop/simple/annotation/MyAspect.java
@@ -0,0 +1,16 @@
+package com.gorden5566.aop.simple.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @author gorden5566
+ * @date 2019-03-17
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.TYPE})
+public @interface MyAspect {
+    String value() default "";
+}
